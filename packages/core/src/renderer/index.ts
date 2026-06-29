e626da0b34c39f5bd1e1e9be56c7ffaa00c953b2
Xuepoo <xuepoofoter@gmail.com>
2026-06-29 13:06:12 +0800

    refactor(core): implement subpath exports and registry pattern for WebGL/WebGPU renderers

---
+++ packages/core/src/renderer/index.ts
@@ -0,0 +1,6 @@
+export * from './CanvasRenderer';
+export * from './SVGRenderer';
+export * from './WebGLPointRenderer';
+export * from './WebGPUParticleSystemManager';
+export * from './IRenderer';
+export * from './colorParse';
